77d25770019fcddb81c74ffa22cfdf37834fc447
Castro-1 <jecastrog@eafit.edu.co>
2023-03-10 10:23:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continious = true;
+mic.interimResults = true;
+mic.lang = "es-co";
 
 function App() {
+  const [isListening, setIsListening] = useState(false);
+  const [note, setNote] = useState("");
+
+  useEffect(() => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log("continue...");
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      setNote("");
+      mic.onend = () => {
+        console.log("stopped mic onclick");
+      };
+    }
+    mic.onstart = () => {
+      console.log("mic on");
+    };
+
+    mic.onresult = (event) => {
+      setNote("");
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      setNote(transcript);
+      mic.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  }, [isListening]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      onClick={() => setIsListening((prevState) => !prevState)}
+    >
+      <p>Click to {isListening ? "disable" : "enable"} transcript...</p>
+      <h1>{note}</h1>
     </div>
   );
 }
